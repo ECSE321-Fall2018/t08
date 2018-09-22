b55aa93f7af2d44c030b0a829ba7fe419d6b7d58
cyrilyared <cyril@yared.info>
2018-09-22 13:39:34 -0400

    Initialize Maven Project with Spring Boot

---
+++ src/main/java/ecse321/t08/intercitycar/IntercityCarApplication.java
@@ -0,0 +1,12 @@
+package ecse321.t08.intercitycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercityCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercityCarApplication.class, args);
+	}
+}
