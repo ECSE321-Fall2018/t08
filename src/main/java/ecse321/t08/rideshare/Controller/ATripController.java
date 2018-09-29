2d62b0ed22f59adeb0c22035817e183585d69ffe
s1o3urn <d4vid4pplepie@gmail.com>
2018-09-29 15:47:31 -0400

    closes #52

---
+++ src/main/java/ecse321/t08/rideshare/Controller/ATripController.java
@@ -0,0 +1,24 @@
+package ecse321.t08.rideshare.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import ecse321.t08.rideshare.Entity.ATrip;
+import ecse321.t08.rideshare.Repository.ATripRepository;
+
+@RestController
+@RequestMapping("api/aTrip")
+public class ATripController {
+
+	 @Autowired
+	    ATripRepository repository;
+
+
+
+	    @RequestMapping(value="/createATrip", method=RequestMethod.POST)
+	    @ResponseBody
+	    public String createATrip(int tripID, double cost, int date, String startLocation, String endLocation){
+	        ATrip newATrip = repository.createATrip(tripID, cost, date, startLocation, endLocation);
+	        return "ATrip " + tripID + " created!";
+	    }
+}
