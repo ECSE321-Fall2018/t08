77bf9790959dae176def89af53c0348a8e1d5b48
cyrilyared <cyril@yared.info>
2018-10-04 10:56:48 -0400

    Add unit tests for rideshareHelperTests

--- src/main/java/ecse321/t08/rideshare/Utility/rideshareHelper.java
+++ src/main/java/ecse321/t08/rideshare/Utility/rideshareHelper.java
@@ -1,7 +1,10 @@
+package ecse321.t08.rideshare.Utility;
+
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-class HelperMethod{
+public class rideshareHelper{
   //method returns a arraylist. To find an element use list.get(). to insert an element at the end use list.add()
   public static ArrayList Tokenizer(String str, String separator){
     StringTokenizer st = new StringTokenizer(str, separator);
@@ -13,7 +16,7 @@ class HelperMethod{
   }
 
   //method returns a string. Put the separator will separate each element of the array in the string
-  public static String Concatenator(ArrayList<String> list, String separator){
+  public static String Concatenator(List<String> list, String separator){
     String concatestring = list.get(0);
     String token = "";
     for(int i = 1; i < list.size(); i++){
