2f6796c4e2e63dfcdcd1ce57781a73b24a4c73c3
s1o3urn <d4vid4pplepie@gmail.com>
2018-10-04 13:37:35 -0400

    fix merge conflicts

--- src/main/java/ecse321/t08/rideshare/controller/ATripController.java
+++ src/main/java/ecse321/t08/rideshare/controller/ATripController.java
@@ -6,6 +6,8 @@ import ecse321.t08.rideshare.repository.ATripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/trip")
 public class ATripController {
