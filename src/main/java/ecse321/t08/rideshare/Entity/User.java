83638df187696a470b3218af17ab0768ac2c7434
projeffboy <jeffery124@gmail.com>
2018-10-04 12:07:43 -0400

    commiter other stuff

--- src/main/java/ecse321/t08/rideshare/Entity/User.java
+++ src/main/java/ecse321/t08/rideshare/Entity/User.java
@@ -9,11 +9,10 @@ import javax.persistence.*;
 @Entity
 @Table(name = "users")
 @NamedQueries({
-        @NamedQuery(name = "User.findUsername", query = "SELECT c FROM User c WHERE c.username LIKE :userName"),
+        @NamedQuery(name = "User.findUsername", query = "SELECT username FROM User WHERE username LIKE :usernameparam"),
         @NamedQuery(name = "User.findAll", query = "SELECT e FROM User e")
 })
 public class User {
-
 
    private int userID;
    private String userName;
@@ -98,5 +97,9 @@ public class User {
       return this.fullName;
    }
 
+   public void setRole(String value) { this.role = value; }
+
+   @Column (name="role")
+   public String getRole() { return this.role; }
 
    }
