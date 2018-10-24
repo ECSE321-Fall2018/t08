ea96e6f196a6bd5c3b9c153876b60ffb85a8f9a1
Zhao Li Ang <zhaoliang10500@gmail.com>
2018-10-24 15:21:43 -0400

    Rename to MainTripDriver

--- frontend-driver/DriverRideShare/app/src/main/java/t08/ecse321/driverrideshare/MainPageDriver.java
+++ frontend-driver/DriverRideShare/app/src/main/java/t08/ecse321/driverrideshare/MainPageDriver.java
@@ -3,11 +3,11 @@ package t08.ecse321.driverrideshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateATrip extends AppCompatActivity {
+public class MainPageDriver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createtrip);
+        setContentView(R.layout.mainpagedriver);
     }
 }
